901ffbd1b264f8b672533cff65ba3fceda1f2075
parhelios <143404279+parhelios@users.noreply.github.com>
2024-03-04 13:17:36 +0100

    created services to temp substitute db

---
+++ Client/Services/MessageService.cs
@@ -0,0 +1,32 @@
+﻿using Shared.Enteties;
+using Shared.Interfaces;
+
+namespace Client.Services;
+
+public class MessageService : IRepository<Message>
+{
+	public Task<IEnumerable<Message>> GetAllAsync()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Message> GetByIdAsync(string id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Message> CreateAsync(Message entity)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task UpdateAsync(string id, Message entity)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task DeleteAsync(string id)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ Client/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Shared.Enteties;
+using Shared.Interfaces;
+
+namespace Client.Services;
+
+public class UserService : IRepository<User>
+{
+	public Task<IEnumerable<User>> GetAllAsync()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<User> GetByIdAsync(string id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<User> CreateAsync(User entity)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task UpdateAsync(string id, User entity)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task DeleteAsync(string id)
+	{
+		throw new NotImplementedException();
+	}
+}
